e0f133ab4585da5a9790f55e34a765f9af49595f
JaewookJung <jaewookjung@JaewookJungui-MacBookPro.local>
2022-02-06 20:10:41 +0900

    Modify: 이중우선순위큐 주석 추가
    Signed-off-by: JaewookJung <jaewookjung@JaewookJungui-MacBookPro.local>

--- week3/이중우선순위큐/stich9208/solution.js
+++ week3/이중우선순위큐/stich9208/solution.js
@@ -1,22 +1,16 @@
 const solution = (operations) => {
   let answer = [];
 
+  //heap part
   const parentIndex = (index) => {
     return Math.floor((index - 1) / 2);
-  };
-
-  const leftChildIndex = (index) => {
-    return index * 2 + 1;
-  };
-
-  const rightChildIndex = (index) => {
-    return index * 2 + 2;
   };
 
   const swap = (array, index1, index2) => {
     [array[index1], array[index2]] = [array[index2], array[index1]];
   };
 
+  //최소값 제거
   const delMin = (array) => {
     let leafCount = Math.ceil(array.length / 2);
     let minNum = array[0];
@@ -30,10 +24,12 @@ const solution = (operations) => {
     array.splice(minIndex, 1);
   };
 
+  //최대값 제거
   const delMax = (array) => {
     array.shift();
   };
 
+  //새로운 원소 추가 최대 heap
   const insert = (array, elem) => {
     array.push(elem);
     if (array.length === 1) {
@@ -49,10 +45,10 @@ const solution = (operations) => {
     }
   };
 
+  //로직 part
+  //함수의 인자로 받은 operations 배열을 순회하며 swtich문을 통해 각 operation에 맞는 로직을 수행
   for (let i = 0; i < operations.length; i++) {
     let operation = operations[i].split(" ");
-    console.log(answer);
-    console.log(operation);
     switch (operation[0]) {
       case "I":
         insert(answer, Number(operation[1]));
