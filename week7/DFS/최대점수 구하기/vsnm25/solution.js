033af5a5c6bf48a7933445e375275eb8bf53870a
Jeong-seungok <vsnm25@naver.com>
2022-04-23 12:16:45 +0900

    Feat: 최대점수 구하기 풀이 주석 추가

--- week7/DFS/최대점수 구하기/vsnm25/solution.js
+++ week7/DFS/최대점수 구하기/vsnm25/solution.js
@@ -1,6 +1,7 @@
 function solution(array, m) {
   const scores = [];
   function dfs(sum, time, index) {
+    // 마지막 index 이고 제한시간보다 작거나 같을 경우
     if (index === array.length) {
       if (time <= m) {
         scores.push(sum);
@@ -8,6 +9,7 @@ function solution(array, m) {
       return;
     }
     const [score, spend] = array[index];
+    // 문제를 풀거나 넘어간 경우로 나눠 재귀
     dfs(sum + score, time + spend, index + 1);
     dfs(sum, time, index + 1);
   }
